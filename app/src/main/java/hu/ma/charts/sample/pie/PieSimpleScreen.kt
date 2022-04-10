20795a752bf327f376389acc5c0e61b154821f7e
Greg <gsteckman@gmail.com>
2022-04-10 12:55:36 -0700

    Remove dependency on Android native canvas.

--- app/src/main/java/hu/ma/charts/sample/pie/PieSimpleScreen.kt
+++ app/src/main/java/hu/ma/charts/sample/pie/PieSimpleScreen.kt
@@ -4,13 +4,20 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
+import hu.ma.charts.legend.data.LegendPosition
 import hu.ma.charts.pie.PieChart
+import hu.ma.charts.pie.data.PieChartData
+import hu.ma.charts.pie.data.PieChartEntry
+import hu.ma.charts.sample.Categories
 import hu.ma.charts.sample.ChartContainer
 import hu.ma.charts.sample.PieSampleData
 import hu.ma.charts.sample.ScreenContainer
+import hu.ma.charts.sample.SimpleColors
 
 @Composable
 fun PieSimpleScreen() {
@@ -24,6 +31,43 @@ fun PieSimpleScreen() {
         title = "Pie - Legend::${it.legendPosition.name}"
       ) {
         PieChart(data = it)
+      }
+    }
+
+    item {
+      ChartContainer(
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 16.dp)
+          .animateContentSize(),
+        title = "Full Pie"
+      ) {
+        PieChart(data = PieSampleData[0], sliceWidth = 1000.dp)
+      }
+    }
+
+    item {
+      val data = PieChartData(
+        entries = listOf(430f).mapIndexed {
+            idx, value ->
+          PieChartEntry(
+            value = value,
+            label = AnnotatedString(Categories[idx])
+          )
+        },
+        colors = SimpleColors,
+        legendPosition = LegendPosition.Start,
+        legendShape = CircleShape,
+      )
+
+      ChartContainer(
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 16.dp)
+          .animateContentSize(),
+        title = "One Slice"
+      ) {
+        PieChart(data = data)
       }
     }
   }
